0ad03c2426cdbbc84dc5d72bbf0ad016f18d6a88
HendiK <hendikrisnanto@rocketmail.com>
2024-06-08 13:24:43 +0700

    Add - Deploy model SVM

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 from flask_cors import CORS
 import numpy as np
 from flask import Flask, request, jsonify, render_template, Response
@@ -8,6 +8,7 @@ import io
 import tensorflow as tf
 import cv2
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -15,7 +16,7 @@ from mediapipe.tasks.python import vision
 
 model_path = 'backend\models\hand_landmarker.task'
 
-alexnet = load_model('./models/alexnet.h5')
+# alexnet = load_model('./models/alexnet.h5')
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -30,34 +31,13 @@ classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 
 def camera_max():
     return 1
-       
+
+
+with open('./models/best_svm_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
 cam_max = camera_max()
 cap = cv2.VideoCapture(cam_max, cv2.CAP_DSHOW)
-
-
-def preProcess(image):
-    gray = tf.image.rgb_to_grayscale(image)
-    gray = tf.squeeze(gray, axis=-1)
-
-    gray_np = tf.image.convert_image_dtype(gray, tf.uint8).numpy()
-
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    cequ_hist = clahe.apply(gray_np)
-
-    gauss_blur = cv2.GaussianBlur(cequ_hist, (3, 3), 20)
-
-    # sobel_x = cv2.Sobel(gauss_blur, cv2.CV_32F, 1, 0, 3)
-    # sobel_y = cv2.Sobel(gauss_blur, cv2.CV_32F, 0, 1, 3)
-
-    # merged_sobel = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
-    # merged_sobel *= 255 / merged_sobel.max()
-
-    resized_image = cv2.resize(gauss_blur, (224, 224))
-
-    resized_image = np.expand_dims(resized_image, axis=-1)
-
-    return resized_image
-
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
@@ -83,14 +63,16 @@ def sign_frame():  # generate frame by frame from camera
                         frame = frame[0:height, diff:width - diff]
 
                     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    results = hands.process(frame_rgb)
+                    results = hands.process(cv2.resize(frame_rgb,(224,224)))
 
                     if results.multi_hand_landmarks:
                         for hand_landmarks in results.multi_hand_landmarks:
-                            x_min = min([landmark.x for landmark in hand_landmarks.landmark])
-                            x_max = max([landmark.x for landmark in hand_landmarks.landmark])
-                            y_min = min([landmark.y for landmark in hand_landmarks.landmark])
-                            y_max = max([landmark.y for landmark in hand_landmarks.landmark])
+                            x_landmark = [landmark.x for landmark in hand_landmarks.landmark]
+                            y_landmark = [landmark.y for landmark in hand_landmarks.landmark]
+                            x_min = min(x_landmark)
+                            x_max = max(x_landmark)
+                            y_min = min(y_landmark)
+                            y_max = max(y_landmark)
                             
                             x_min = int(x_min * width)
                             x_max = int(x_max * width)
@@ -104,20 +86,34 @@ def sign_frame():  # generate frame by frame from camera
                             y_min_adj = max(0, y_min - 50)
                             y_max_adj = min(height, y_max + 50)
                             roi = frame[y_min_adj:y_max_adj, x_min_adj:x_max_adj]
-                            
-                            roi_preprocessed = preProcess(roi)
-                            roi_preprocessed = np.expand_dims(roi_preprocessed, axis=0)
+                            hand_data = {}
+                            for idx1, landmark in enumerate(hand_landmarks.landmark):
+                                # print("x",landmark.x)
+                                # print("0",hand_landmarks.landmark[0].x)
+                                if(idx1==0):continue
+                                relative_x = landmark.x - hand_landmarks.landmark[0].x
+                                relative_y = landmark.y - hand_landmarks.landmark[0].y
+                                # print(type(idx1), type(relative_x),type(relative_y))
+                                hand_data[f"Landmark_{idx1 + 1}_X"] = [relative_x]
+                                hand_data[f"Landmark_{idx1 + 1}_Y"] = [relative_y]
+                            # roi_preprocessed = preProcess(roi)
+                            # roi_preprocessed = np.expand_dims(roi_preprocessed, axis=0)
 
                             # plt.imshow(roi_preprocessed.squeeze(), cmap='gray')
                             # plt.title('Preprocessed Image')
                             # plt.show()
                             # plt.close()
+                            # relative_x = landmark.x - hand_landmarks.landmark[0].x
+                            # relative_y = landmark.y - hand_landmarks.landmark[0].y
 
                             target_size = (224, 224)
                             # print(roi_preprocessed)
-                            prediction = alexnet.predict(roi_preprocessed)
-                            predicted_class = np.argmax(prediction,axis=1)
-                            cv2.putText(frame, f'Class: {classes[predicted_class[0]]}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                            # prediction = alexnet.predict(roi_preprocessed)
+                            # print(hand_data)
+                            prediction = model.predict(pd.DataFrame(hand_data))
+                            # print(prediction)
+                            # predicted_class = np.argmax(prediction)
+                            cv2.putText(frame, f'Class: {prediction[0]}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
                     ret, buffer = cv2.imencode('.jpg', frame)
                     frame_bytes = buffer.tobytes()
